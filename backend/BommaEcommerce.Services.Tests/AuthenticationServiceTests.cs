8107e2a985fccf7210443196c40e6757c5617be5
turhhuhu <-d>
2021-03-30 22:35:04 +0300

    add service and domain test framework #22

---
+++ backend/BommaEcommerce.Services.Tests/AuthenticationServiceTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace BommaEcommerce.Services.Tests
+{
+    public class AuthenticationServiceTests
+    {
+        
+    }
+}
---
+++ backend/BommaEcommerce.Services.Tests/StoresServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+
+namespace BommaEcommerce.Services.Tests
+{
+    public class StoresServiceTests
+    {
+        
+    }
+}
