3cf02cba748d899ecd857b354583abf097c48288
BennySkidanov <skidanov@post.bgu.ac.il>
2021-04-04 17:58:49 +0300

    wrong branch

--- backend/BoomaEcommerce.Services/Products/ProductsService.cs
+++ backend/BoomaEcommerce.Services/Products/ProductsService.cs
@@ -2,27 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
-using BoomaEcommerce.Data;
-using BoomaEcommerce.Domain;
 using BoomaEcommerce.Services.DTO;
-using Microsoft.Extensions.Logging;
+
 
 namespace BoomaEcommerce.Services.Products
 {
     public class ProductsService : IProductsService
     {
-        private readonly ILogger<ProductsService> _logger;
-        private readonly IMapper _mapper;
-        private readonly IRepository<Product> _productRepo;
         public Task CreateStoreProductAsync(ProductDto product)
         {
             throw new NotImplementedException();
-        }
-
-        public Task GetProductsFromStore(Guid storeGuid)
-        {
-            var products = _
         }
 
         public Task<IReadOnlyCollection<ProductDto>> GetAllProductsAsync()
