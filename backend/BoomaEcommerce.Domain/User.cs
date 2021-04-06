f5caa76e2d3e7683a6e05aaa85bdcfb49636dfd7
Arik Shapiro <aryesh@post.bgu.ac.il>
2021-04-06 21:24:03 +0300

    remove user constructor that was breaking build #17

--- backend/BoomaEcommerce.Domain/User.cs
+++ backend/BoomaEcommerce.Domain/User.cs
@@ -9,12 +9,6 @@ namespace BoomaEcommerce.Domain
     public class User : BaseEntity
     {
         public string UserName { get; set; }
-        public User(string userName, Guid guid)
-        {
-            this.UserName = userName;
-            this.Guid = guid;
-        }
-
         public string Name { get; set; }
         public string LastName { get; set; }
 
