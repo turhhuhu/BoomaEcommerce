26511b5ea3f6f08cd8fa12f44eb01dedc9c893b3
BennySkidanov <skidanov@post.bgu.ac.il>
2021-04-07 10:41:50 +0300

    added get store test

--- backend/BommaEcommerce.Services.Tests/StoresServiceTests.cs
+++ backend/BommaEcommerce.Services.Tests/StoresServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoFixture;
 using AutoMapper;
 using BommaEcommerce.Services.Tests;
 using BoomaEcommerce.Domain;
@@ -24,6 +25,7 @@ namespace BoomaEcommerce.Services.Tests
             cfg.AddProfile(new DtoToDomainProfile());
         });
         IMapper mapper = config.CreateMapper();
+        private IFixture _fixture = new Fixture();
        
         [Fact]
         public async void GetStoreTest()
@@ -56,6 +58,75 @@ namespace BoomaEcommerce.Services.Tests
             expectedRes3.Add(mapper.Map<StoreDto>(s2));
             expectedRes3.Add(mapper.Map<StoreDto>(s3));
             res3.ToList().Should().BeEquivalentTo(expectedRes3);
+        }
+
+        [Fact]
+        public async void GetStorePurchaseHistoryTest()
+        {
+            Mock<ILogger<StoresService>> loggerMock = new Mock<ILogger<StoresService>>();
+            Store s1 = new() {};
+            Store s2 = new() {};
+            Store s3 = new() {};
+
+            //store1 purchase 
+                //p1 
+            var pr1 = _fixture.Build<PurchaseProduct>().Create();
+            var pr2 = _fixture.Build<PurchaseProduct>().Create();
+            var pr3 = _fixture.Build<PurchaseProduct>().Create();
+
+            var prList1 = new List<PurchaseProduct>();
+            prList1.Add(pr1);
+            prList1.Add(pr2);
+            prList1.Add(pr3);
+
+            StorePurchase p1 = new() {ProductsPurchases = prList1 , Store = s1};
+               
+                //p2
+            var pr4 = _fixture.Build<PurchaseProduct>().Create();
+            var pr5 = _fixture.Build<PurchaseProduct>().Create(); 
+
+            var prList2 = new List<PurchaseProduct>();
+            prList2.Add(pr4);
+            prList2.Add(pr5);
+            
+
+            StorePurchase p2 = new() { ProductsPurchases = prList2, Store = s1 };
+
+            //store2 purchase 
+            var pr6 = _fixture.Build<PurchaseProduct>().Create();
+            var prList3 = new List<PurchaseProduct>();
+            prList3.Add(pr6);
+
+            StorePurchase p3 = new() { ProductsPurchases = prList3, Store = s2 };
+
+            _EntitiesStorePurchases.Add(p1.Guid, p1);
+            _EntitiesStorePurchases.Add(p2.Guid, p2);
+            _EntitiesStorePurchases.Add(p3.Guid, p3);
+
+            _EntitiesStores.Add(s1.Guid,s1);
+            _EntitiesStores.Add(s2.Guid, s2);
+
+            var storesRepo = DalMockFactory.MockRepository(_EntitiesStores);
+            var storesPurchaseRepo = DalMockFactory.MockRepository(_EntitiesStorePurchases);
+            var storesService = new StoresService(loggerMock.Object, mapper, storesRepo.Object, storesPurchaseRepo.Object);
+
+            var res1 = await storesService.GetStorePurchaseHistory(s1.Guid);
+            var expectedRes1 = new List<StorePurchaseDto>();
+            expectedRes1.Add(mapper.Map<StorePurchaseDto>(p1));
+            expectedRes1.Add(mapper.Map<StorePurchaseDto>(p2));
+
+            res1.ToList().Should().BeEquivalentTo(expectedRes1);
+
+            var res2 = await storesService.GetStorePurchaseHistory(s2.Guid);
+            var expectedRes2 = new List<StorePurchaseDto>();
+            expectedRes2.Add(mapper.Map<StorePurchaseDto>(p3));
+
+            res2.ToList().Should().BeEquivalentTo(expectedRes2);
+
+            var res3 = await storesService.GetStorePurchaseHistory(s3.Guid);
+            res3.Should().BeEmpty();
+
+
         }
     }
 }
