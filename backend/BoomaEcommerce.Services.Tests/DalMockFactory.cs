5e3e2efa27f741ba7c5369669801f02b84458df2
turhhuhu <-d>
2021-04-07 13:55:05 +0300

    change purchase service to use unitOfWork #9 update fitting service test to work with the new change #25

--- backend/BoomaEcommerce.Services.Tests/DalMockFactory.cs
+++ backend/BoomaEcommerce.Services.Tests/DalMockFactory.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using BoomaEcommerce.Core;
 using BoomaEcommerce.Data;
 using BoomaEcommerce.Domain;
-using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
-using FluentAssertions;
 using Microsoft.AspNetCore.Identity;
 using Moq;
-using Expression = Castle.DynamicProxy.Generators.Emitters.SimpleAST.Expression;
 
-namespace BommaEcommerce.Services.Tests
+namespace BoomaEcommerce.Services.Tests
 {
     public static class DalMockFactory
     {
