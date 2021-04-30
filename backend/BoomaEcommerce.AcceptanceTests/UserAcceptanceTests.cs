cdc003397563c4b0f28c7d9fe45d1e27d5784ca7
Benny-Skidanov <48641374+BennySkidanov@users.noreply.github.com>
2021-04-30 17:28:58 +0300

    Shopping cart edition tests are added

--- backend/BoomaEcommerce.AcceptanceTests/UserAcceptanceTests.cs
+++ backend/BoomaEcommerce.AcceptanceTests/UserAcceptanceTests.cs
@@ -302,6 +302,69 @@ namespace BoomaEcommerce.AcceptanceTests
             purchaseWasSuccessful.Should().BeFalse();
         }
 
+        [Fact]
+        public async Task DeletePurchaseProductFromShoppingBasketAsync_UpdatesContentOfCart_WhenDetailsAreValid()
+        {
+            // Arrange
+            var shoppingCart = await _usersService.GetShoppingCartAsync(UserGuid);
+
+            var fixtureShoppingBasket = _fixture
+                .Build<ShoppingBasketDto>()
+                .With(s => s.StoreGuid, _store_withGuid.Guid)
+                .With(s => s.PurchaseProducts, purchase_product_lst)
+                .Without(s => s.Guid)
+                .Create();
+
+            var shoppingBasket = await _usersService.CreateShoppingBasketAsync(shoppingCart.Guid, fixtureShoppingBasket);
+            var guidToDelete = ((await _usersService.GetShoppingCartAsync(UserGuid)).Baskets.First())
+                .PurchaseProducts[0].Guid;
+
+            var countBeforeEditing = shoppingBasket.PurchaseProducts.Count;
+
+            // Act
+            var success =
+                await _usersService.DeletePurchaseProductFromShoppingBasketAsync(shoppingBasket.Guid,
+                    guidToDelete);
+
+            var shoppingCartUpdated = await _usersService.GetShoppingCartAsync(UserGuid);
+
+            var countAfterEditing = shoppingCartUpdated.Baskets.First().PurchaseProducts.Count;
+
+            // Assert
+            (countAfterEditing - countBeforeEditing).Should().Be(-1);
+
+        }
+
+        [Fact]
+        public async Task DeletePurchaseProductFromShoppingBasketAsync_UpdatesContentOfCartDoesNotHappen_WhenDetailsAreNotValid()
+        {
+            // Arrange
+            var shoppingCart = await _usersService.GetShoppingCartAsync(UserGuid);
+
+            var fixtureShoppingBasket = _fixture
+                .Build<ShoppingBasketDto>()
+                .With(s => s.StoreGuid, _store_withGuid.Guid)
+                .With(s => s.PurchaseProducts, purchase_product_lst)
+                .Without(s => s.Guid)
+                .Create();
+
+            var shoppingBasket = await _usersService.CreateShoppingBasketAsync(shoppingCart.Guid, fixtureShoppingBasket);
+            var guidToDelete = ((await _usersService.GetShoppingCartAsync(UserGuid)).Baskets.First())
+                .PurchaseProducts[0].Guid;
+
+            var countBeforeEditing = shoppingBasket.PurchaseProducts.Count;
+
+            // Act
+            var success =
+                await _usersService.DeletePurchaseProductFromShoppingBasketAsync(shoppingBasket.Guid,
+                    Guid.NewGuid());
+
+            // Assert
+            success.Should().BeFalse();
+
+        }
+
+
 
         public Task DisposeAsync()
         {
