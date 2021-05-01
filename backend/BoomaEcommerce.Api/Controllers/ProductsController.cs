82e859ddd5dbc7a285d9f9d7b7b0ee52403e1f2e
Arik Shapiro <arikshaapiro@gmail.com>
2021-05-01 15:05:10 +0300

    finish user controller unit tests #160

--- backend/BoomaEcommerce.Api/Controllers/ProductsController.cs
+++ backend/BoomaEcommerce.Api/Controllers/ProductsController.cs
@@ -72,7 +72,8 @@ namespace BoomaEcommerce.Api.Controllers
                 return BadRequest();
             }
 
-            return Ok(productResult);
+            var locationUrl = $"{this.GetBaseUrl()}/products/{productResult.Guid}";
+            return Created(locationUrl, productResult);
         }
 
         [Authorize]
