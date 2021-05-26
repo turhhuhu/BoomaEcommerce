5cdb8075e5e4b3284e6f361261ade0a02908b885
BennySkidanov <skidanov@post.bgu.ac.il>
2021-05-26 23:24:16 +0300

    added policy tests

--- backend/BoomaEcommerce.AcceptanceTests/PolicyAcceptanceTests.cs
+++ backend/BoomaEcommerce.AcceptanceTests/PolicyAcceptanceTests.cs
@@ -79,7 +79,7 @@ namespace BoomaEcommerce.AcceptanceTests
             purchase_product2 = _fixture.Build<PurchaseProductDto>()
                                           .With(pp => pp.ProductGuid, product2_withGuid.Guid)
                                           .Without(pp => pp.Guid)
-                                          .With(pp => pp.Price, product2_withGuid.Price)
+                                          .With(pp => pp.Price, 4 * product2_withGuid.Price)
                                           .With(pp => pp.Amount, 4)
                                           .Create();
             var purchase_product_lst = new List<PurchaseProductDto>();
@@ -128,7 +128,7 @@ namespace BoomaEcommerce.AcceptanceTests
         }
     
         [Fact] 
-        private async Task test1()
+        private async Task MaxCategoryAmountPolicyDtoTest_ShouldThrowPolicyValidationException()
         {
             // Arange
 
@@ -180,6 +180,117 @@ namespace BoomaEcommerce.AcceptanceTests
             // Assert
             await result.Should().ThrowAsync<PolicyValidationException>();
         }
+
+
+        [Fact]
+        private async Task MaxCategoryAmountPolicyDtoTest_ShouldMakePurchase()
+        {
+            // Arange
+
+            //get first store policy
+            var first_store_policy = await _storeService.GetPolicyAsync(_store_withGuid.Guid);
+
+            // create new policy 
+            _fixture.Customize<CategoryAmountPolicyDto>(p => p.With(pp => pp.Type, PolicyType.MaxCategoryAmount)
+                                                              .With(pp => pp.Category, "Diary")
+                                                              .With(pp => pp.Amount, 3)
+                                                              .Without(pp => pp.Guid));
+            var policy1 = _fixture.Create<CategoryAmountPolicyDto>();
+
+            await _storeService.CreatePurchasePolicyAsync(_store_withGuid.Guid, policy1);
+
+            //create purchase product 
+            purchase_product1 = _fixture.Build<PurchaseProductDto>()
+                                            .With(pp => pp.ProductGuid, product1_withGuid.Guid)
+                                            .Without(pp => pp.Guid)
+                                            .With(pp => pp.Price, product1_withGuid.Price * 3)
+                                            .With(pp => pp.Amount, 3)
+                                            .Create();
+
+
+            var purchaseProductList = new List<PurchaseProductDto>();
+            purchaseProductList.Add(purchase_product1);
+
+            var storePurchase = _fixture.Build<StorePurchaseDto>()
+                .With(sp => sp.StoreGuid, _store_withGuid.Guid)
+                .With(sp => sp.BuyerGuid, UserGuid)
+                .With(sp => sp.PurchaseProducts, purchaseProductList)
+                .Without(sp => sp.Guid)
+                .With(sp => sp.TotalPrice, purchase_product1.Price)
+                .Create();
+
+            var store_purchase_lst = new List<StorePurchaseDto>();
+            store_purchase_lst.Add(storePurchase);
+
+            purchase = _fixture.Build<PurchaseDto>()
+                .With(p => p.BuyerGuid, UserGuid)
+                .With(p => p.StorePurchases, store_purchase_lst)
+                .Without(p => p.Guid)
+                .With(p => p.TotalPrice, storePurchase.TotalPrice)
+                .Create();
+
+            //Act 
+            var result =  await _purchaseService.CreatePurchaseAsync(purchase);
+
+            // Assert
+            result.Should().NotBeNull();
+            var product = await _storeService.GetStoreProductAsync(product1_withGuid.Guid);
+            product.Amount.Should().Be(7);
+        }
+
+
+        [Fact]
+        private async Task MinCategoryAmountPolicyDtoTest_ShouldThrowPolicyValidationException()
+        {
+            // Arange
+
+           
+            // create new policy 
+            _fixture.Customize<CategoryAmountPolicyDto>(p => p.With(pp => pp.Type, PolicyType.MinCategoryAmount)
+                                                              .With(pp => pp.Category, "Diary")
+                                                              .With(pp => pp.Amount, 2)
+                                                              .Without(pp => pp.Guid));
+            var policy1 = _fixture.Create<CategoryAmountPolicyDto>();
+
+            await _storeService.CreatePurchasePolicyAsync(_store_withGuid.Guid, policy1);
+
+            //create purchase product 
+            purchase_product1 = _fixture.Build<PurchaseProductDto>()
+                                            .With(pp => pp.ProductGuid, product1_withGuid.Guid)
+                                            .Without(pp => pp.Guid)
+                                            .With(pp => pp.Price, product1_withGuid.Price * 1)
+                                            .With(pp => pp.Amount, 1)
+                                            .Create();
+
+
+            var purchaseProductList = new List<PurchaseProductDto>();
+            purchaseProductList.Add(purchase_product1);
+
+            var storePurchase = _fixture.Build<StorePurchaseDto>()
+                .With(sp => sp.StoreGuid, _store_withGuid.Guid)
+                .With(sp => sp.BuyerGuid, UserGuid)
+                .With(sp => sp.PurchaseProducts, purchaseProductList)
+                .Without(sp => sp.Guid)
+                .With(sp => sp.TotalPrice, purchase_product1.Price)
+                .Create();
+
+            var store_purchase_lst = new List<StorePurchaseDto>();
+            store_purchase_lst.Add(storePurchase);
+
+            purchase = _fixture.Build<PurchaseDto>()
+                .With(p => p.BuyerGuid, UserGuid)
+                .With(p => p.StorePurchases, store_purchase_lst)
+                .Without(p => p.Guid)
+                .With(p => p.TotalPrice, storePurchase.TotalPrice)
+                .Create();
+
+            //Act 
+            var result = _purchaseService.Awaiting(service => service.CreatePurchaseAsync(purchase));
+
+            // Assert
+            await result.Should().ThrowAsync<PolicyValidationException>();
+        }
+
 
         public Task DisposeAsync()
         {
