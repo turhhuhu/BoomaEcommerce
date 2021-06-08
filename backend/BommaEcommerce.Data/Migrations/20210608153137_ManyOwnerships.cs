49154e1d01ae4d7ea1bec8b6a635aefb25851ee7
Arik Shapiro <arikshaapiro@gmail.com>
2021-06-08 22:01:20 +0300

    add migrations

---
+++ backend/BommaEcommerce.Data/Migrations/20210608153137_ManyOwnerships.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoomaEcommerce.Data.Migrations
+{
+    public partial class ManyOwnerships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "StoreOwnershipGuid",
+                table: "StoreOwnerships",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreOwnerships_StoreOwnershipGuid",
+                table: "StoreOwnerships",
+                column: "StoreOwnershipGuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoreOwnerships_StoreOwnerships_StoreOwnershipGuid",
+                table: "StoreOwnerships",
+                column: "StoreOwnershipGuid",
+                principalTable: "StoreOwnerships",
+                principalColumn: "Guid",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoreOwnerships_StoreOwnerships_StoreOwnershipGuid",
+                table: "StoreOwnerships");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StoreOwnerships_StoreOwnershipGuid",
+                table: "StoreOwnerships");
+
+            migrationBuilder.DropColumn(
+                name: "StoreOwnershipGuid",
+                table: "StoreOwnerships");
+        }
+    }
+}
