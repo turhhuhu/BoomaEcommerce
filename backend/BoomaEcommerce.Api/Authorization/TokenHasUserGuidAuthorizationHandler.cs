ea63ef30d2878d7dcd09d0b0288d26f2f0f3a142
Arik Shapiro <arikshaapiro@gmail.com>
2021-06-11 08:45:07 +0300

    add token check every keep-alive in NotificationHub

--- backend/BoomaEcommerce.Api/Authorization/TokenHasUserGuidAuthorizationHandler.cs
+++ backend/BoomaEcommerce.Api/Authorization/TokenHasUserGuidAuthorizationHandler.cs
@@ -5,18 +5,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoomaEcommerce.Services.Settings;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace BoomaEcommerce.Api.Authorization
 {
     public class TokenHasUserGuidAuthorizationHandler : AuthorizationHandler<TokenHasUserGuidRequirement>
     {
+        private readonly JwtSettings _jwtSettings;
 
-        public TokenHasUserGuidAuthorizationHandler()
+        public TokenHasUserGuidAuthorizationHandler(IOptions<JwtSettings> jwtOptions)
         {
+            _jwtSettings = jwtOptions.Value;
         }
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, TokenHasUserGuidRequirement requirement)
         {
-            if (context.User.TryGetUserGuid(out _))
+            var expirationClaim = context.User?.Claims.SingleOrDefault(x => x.Type == JwtRegisteredClaimNames.Exp)?.Value;
+            if (expirationClaim == null)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+            var expirationDateUnix = long.Parse(expirationClaim);
+            var expirationDateTimeUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddSeconds(expirationDateUnix);
+
+            if (context.User.TryGetUserGuid(out _) && expirationDateTimeUtc > DateTime.UtcNow)
             {
                 context.Succeed(requirement);
             }
--- backend/BoomaEcommerce.Api/Hubs/NotificationHub.cs
+++ backend/BoomaEcommerce.Api/Hubs/NotificationHub.cs
@@ -7,9 +7,17 @@ using System.Threading.Tasks;
 using BoomaEcommerce.Core;
 using BoomaEcommerce.Services;
 using BoomaEcommerce.Services.DTO;
+using BoomaEcommerce.Services.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace BoomaEcommerce.Api.Hubs
 {
@@ -18,11 +26,13 @@ namespace BoomaEcommerce.Api.Hubs
     {
         private readonly ILogger<NotificationHub> _logger;
         private readonly IConnectionContainer _connectionContainer;
+        private readonly JwtSettings _jwtSettings;
 
-        public NotificationHub(ILogger<NotificationHub> logger, IConnectionContainer connectionContainer)
+        public NotificationHub(ILogger<NotificationHub> logger, IConnectionContainer connectionContainer, IOptions<JwtSettings> jwtSettings)
         {
             _logger = logger;
             _connectionContainer = connectionContainer;
+            _jwtSettings = jwtSettings.Value;
         }
 
         public override Task OnConnectedAsync()
@@ -30,7 +40,54 @@ namespace BoomaEcommerce.Api.Hubs
             var userGuid = Context.User.GetUserGuid();
             _logger.LogInformation($"User with Guid {userGuid} connected to notifications hub.");
             _connectionContainer.SaveConnection(userGuid, Context.ConnectionId);
+
+            TerminateConnectionOnTokenExpiry();
+
             return base.OnConnectedAsync();
+        }
+
+        private void TerminateConnectionOnTokenExpiry()
+        {
+            var context = Context.GetHttpContext();
+            var feature = Context.GetHttpContext()?.Features.Get<IConnectionHeartbeatFeature>();
+            if (feature == null)
+            {
+                return;
+            }
+
+            if (context == null)
+            {
+                throw new InvalidOperationException("The HTTP context cannot be resolved.");
+            }
+
+            // Extract the authentication ticket from the access token.
+            // Note: this operation should be cheap as the authentication result
+            // was already computed when SignalR invoked the authentication handler
+            // and automatically cached by AuthenticationHandler.AuthenticateAsync().
+            var connectionId = Context.ConnectionId;
+            var expirationClaim = Context.User?.Claims.SingleOrDefault(x => x.Type == JwtRegisteredClaimNames.Exp)?.Value;
+            if (expirationClaim == null)
+            {
+                _logger.LogWarning($"Token for connection id {connectionId} is missing expiry time, aborting.");
+                Context.Abort();
+                return;
+            }
+            var expirationDateUnix = long.Parse(expirationClaim);
+            var expirationDateTimeUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddSeconds(expirationDateUnix);
+
+            feature.OnHeartbeat(state =>
+            {
+                var (exp, httpContext) = ((DateTime, HttpContext))state;
+
+                if (exp < DateTime.UtcNow)
+                {
+                    _logger.LogInformation($"Token for connection id {connectionId} has expired, aborting.");
+                    httpContext.Abort();
+                }
+                // Ensure the access token token is still valid.
+                // If it's not, abort the connection immediately.
+            }, (expirationDateTimeUtc, context));
         }
 
         public override Task OnDisconnectedAsync(Exception? exception)
